3a507e8ddaa4b2a1bbc0d3acaf368c9e9402fe7f
kkamara <kamaracomputers@gmail.com>
2023-12-30 18:29:58 +0000

    Rendering list of products

--- context/index.js
+++ context/index.js
@@ -1,25 +1,30 @@
 import { createContext, useEffect, useState, } from "react"
 
-const Context = createContext(null)
+export const Context = createContext(null)
 
 const ProductContext = ({children}) => {
     const [products, setProducts] = useState([])
 
+    const [loading, setLoading] = useState(false)
+
     useEffect(() => {
+        setLoading(true)
         async function getProductsFromApi() {
             const apiRes = await fetch("https://dummyjson.com/products")
             const finalResult = await apiRes.json()
             if (finalResult) {
+                // setTimeout(() => {
+                //     setLoading(false)
+                // }, 2000)
                 setProducts(finalResult.products)
+                setLoading(false)
             }
         }
 
         getProductsFromApi()
     }, [])
 
-    console.log(products)
-
-    return <Context.Provider value={{ products, }}>
+    return <Context.Provider value={{ products, loading, }}>
         {children}
     </Context.Provider>
 }
--- screens/productListing/index.jsx
+++ screens/productListing/index.jsx
@@ -1,10 +1,42 @@
-import { View, Text, } from "react-native";
+import { useContext, } from "react"
+import { 
+    View, 
+    Text, 
+    ActivityIndicator,
+    StyleSheet,
+    FlatList,
+} from "react-native"
+import { Context, } from "../../context"
 
 
 export default function ProductListing() {
+    const { loading, products, } = useContext(Context)
+
+    if (loading) {
+        return (
+            <ActivityIndicator style={styles.loader} color={"red"} size={"large"} />
+        )
+    }
+
     return (
         <View>
+            <FlatList
+                data={products}
+                renderItem={(itemData) => <Text>
+                    {itemData.item.title}
+                </Text>}
+                keyExtractor={(itemData) => itemData.id}
+                numColumns={2}
+            />
             <Text>ProductListing</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    loader: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+})
