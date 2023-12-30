0409e2b685c87d20470798a00ce0a8dad7d64701
kkamara <kamaracomputers@gmail.com>
2023-12-30 16:59:02 +0000

    Add React Native Bottom Tabs navigation component

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, View, } from 'react-native'
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, View, } from "react-native"
 import ProductListing from "./screens/productListing"
 import Favourites from "./screens/favourites"
 import ProductDetails from "./screens/productDetails"
 import { NavigationContainer, } from "@react-navigation/native"
 import { createNativeStackNavigator, } from "@react-navigation/native-stack"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 
 const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
+
+function BottomTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="productListing" component={ProductListing} />
+      <Tab.Screen name="favourites" component={Favourites} />
+    </Tab.Navigator>
+  )
+}
 
 export default function App() {
   return (
@@ -14,9 +25,8 @@ export default function App() {
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="productListing" component={ProductListing} />
+          <Stack.Screen name="bottomtabs" component={BottomTabs} />
           <Stack.Screen name="productDetails" component={ProductDetails} />
-          <Stack.Screen name="favourites" component={Favourites} />
         </Stack.Navigator>
       </NavigationContainer>
     </View>
