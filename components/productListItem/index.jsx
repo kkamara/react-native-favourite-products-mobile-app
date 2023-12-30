a598dc798f21ece9d0f9cfc3106d3d09aeb012c0
kkamara <kamaracomputers@gmail.com>
2023-12-30 18:45:28 +0000

    Designing product-list-item component

--- components/productListItem/index.jsx
+++ components/productListItem/index.jsx
@@ -1,10 +1,46 @@
-import { View, Text, } from "react-native"
+import {
+    Pressable,
+    View,
+    Text,
+    StyleSheet,
+} from "react-native"
 
 
-export default function ProductListItem() {
+export default function ProductListItem({
+    title,
+    onPress,
+    bgColor,
+}) {
     return (
-        <View>
-            <Text>ProductListItem</Text>
+        <View style={styles.productItemOuterContainer}>
+            <Pressable onPress={onPress} style={{ ...styles.pressableView, backgroundColor: bgColor, }}>
+                <View style={styles.productItemInnerContainer}>
+                    <Text style={styles.title}>{title}</Text>
+                </View>
+            </Pressable>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    productItemOuterContainer: {
+        flex: 1,
+        margin: 16,
+        height: 160,
+        borderRadius: 8,
+    },
+    pressableView: {
+        flex: 1,
+    },
+    productItemInnerContainer: {
+        flex: 1,
+        padding: 15,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 20,
+        color: "#000",
+    },
+})
--- screens/productListing/index.jsx
+++ screens/productListing/index.jsx
@@ -7,7 +7,18 @@ import {
     FlatList,
 } from "react-native"
 import { Context, } from "../../context"
+import ProductListItem from "../../components/productListItem"
 
+function createRandomColor() {
+    let letters = "0123456789ABCDEF"
+    let color = "#"
+
+    for (let i=0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+
+    return color
+}
 
 export default function ProductListing() {
     const { loading, products, } = useContext(Context)
@@ -18,13 +29,17 @@ export default function ProductListing() {
         )
     }
 
+    const handleOnPress = () => {}
+
     return (
         <View>
             <FlatList
                 data={products}
-                renderItem={(itemData) => <Text>
-                    {itemData.item.title}
-                </Text>}
+                renderItem={(itemData) => <ProductListItem
+                    title={itemData.item.title}
+                    bgColor={createRandomColor()}
+                    onPress={handleOnPress}                   
+                />}
                 keyExtractor={(itemData) => itemData.id}
                 numColumns={2}
             />
